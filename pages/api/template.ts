9ca665bc0938a0faa3858ec2e945b0227daaefd6
LOGANLEEE <logan_lee@kakao.com>
2022-11-24 23:53:17 +0000

    add api

--- pages/api/template.ts
+++ pages/api/template.ts
@@ -1,10 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { accessor } from '../../lib/crawl/logic/dc';
-
-// const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	const { data } = await accessor();
-	res.status(200).json({ data });
+	res.status(200).json({ message: ' template' });
 }
