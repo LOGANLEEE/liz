29caea8a41e47ca141c3cc27bb0f78c6870f792c
LOGANLEEE <logan_lee@kakao.com>
2022-11-24 23:52:42 +0000

    add crawling logic

--- lib/crawl/logic/dc.ts
+++ lib/crawl/logic/dc.ts
@@ -1,12 +1,14 @@
 import { JSDOM } from 'jsdom';
-import { delay, _axios } from '../../util';
+import { _axios } from '../../axiosInstance';
+import { delay, masking } from '../../util';
 import { names, targetInfo } from '../targetInfo';
 
-export const accessor = async () => {
+export const DCINSIDEAccessor = async (): Promise<{ count: number; isError: boolean; name: string; message: '' }> => {
 	// get target Info
-	const { author, hit, link, name, page, range, rangeGap, targetUrl, targetIndex } = targetInfo.filter(
+	const { author, hit, link, name, page, range, rangeGap, targetUrl, targetIndex, garbage, targetHostName } = targetInfo.filter(
 		({ name }) => name === names.dc
 	)[0];
+
 	const tempHolder = [];
 
 	for (let pageCount = page[0]; pageCount <= page[1]; pageCount += 1) {
@@ -16,40 +18,33 @@ export const accessor = async () => {
 		if (!error && data) {
 			const fragment = JSDOM.fragment(data);
 			for (let idx = range[0]; idx <= range[1]; idx += rangeGap) {
+				// remove garbage tag
+				garbage(idx).forEach((target) => {
+					fragment.querySelector(target)?.remove();
+				});
+
 				const postIndex = fragment.querySelector(targetIndex(idx))?.textContent || 'null';
 
 				if (isNaN(parseInt(postIndex))) continue;
 
-				const _link = fragment.querySelector(link(idx))?.getAttribute('href');
-				const _author = fragment.querySelector(author(idx))?.getAttribute('title');
-				const _hit = fragment.querySelector(hit(idx))?.textContent;
-				tempHolder.push({ postIndex, _link, _author, _hit });
+				const _link = fragment.querySelector(link(idx))?.getAttribute('href') || masking;
+				const _title = fragment.querySelector(link(idx))?.textContent || masking;
+				const _author = fragment.querySelector(author(idx))?.getAttribute('title') || masking;
+				const _hit = parseInt(fragment.querySelector(hit(idx))?.textContent || '---');
+
+				if (_title === masking) return { count: 0, isError: true, name, message: '' };
+				if (_link === masking) return { count: 0, isError: true, name, message: '' };
+				if (_author === masking) return { count: 0, isError: true, name, message: '' };
+				if (isNaN(_hit)) return { count: 0, isError: true, name, message: '' };
+
+				tempHolder.push({ link: `${targetHostName}${_link}`, author: _author, hit: _hit, name, title: _title, mark: false });
 			}
 		}
 	}
+	if (tempHolder.length < 1) return { count: 0, isError: true, name, message: '' };
 
-	// targetURLs.map(async ({ targetName, targetUrl: url }) => {
-	// const test = await JSDOM.fromURL(url, {}).then((dom) => {
-	// 	return dom.serialize();
-	// });
-	// 	const { data, error } = await _axios.get(url).catch((error) => error);
-	// 	if (!error && data) {
-	// 		const fragment = JSDOM.fragment(data);
-	// 		return targetInfo
-	// 			.filter(({ name }) => name === targetName)
-	// 			.map(({ author, hit, link, name, page, range, rangeGap }) => {
-	// 				const tempHolder = [];
-	// 				for (let idx = range[0]; idx <= range[1]; idx += rangeGap) {
-	// 					const _link = fragment.querySelector(link(idx))?.getAttribute('href');
-	// 					const _author = fragment.querySelector(author(idx))?.getAttribute('title');
-	// 					const _hit = fragment.querySelector(hit(idx));
-	// 					tempHolder.push({ _link, _author, _hit });
-	// 					return tempHolder;
-	// 				}
-	// 			});
-	// 	}
-	// })
-	// );
-
-	return { data: tempHolder };
+	await _prisma.$connect();
+	const { count } = await _prisma.fresh_post.createMany({ data: tempHolder });
+	await _prisma.$disconnect();
+	return { count, isError: false, message: '', name };
 };
---
+++ lib/crawl/logic/posts.ts
@@ -0,0 +1,6 @@
+import { _prisma } from '../../../prisma/prismaInstance';
+
+export const getFreshPost = async () => {
+	const list = await _prisma.fresh_post.findMany({ where: { mark: false } });
+	return list;
+};
