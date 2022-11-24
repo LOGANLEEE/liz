513eb0fa08e4f65066babdb67639562f061eaded
LOGANLEEE <logan_lee@kakao.com>
2022-11-24 23:52:56 +0000

    add

--- lib/crawl/targetInfo.ts
+++ lib/crawl/targetInfo.ts
@@ -3,10 +3,14 @@ export const names = { dc: 'DC' };
 export const targetInfo = [
 	{
 		name: names.dc,
+		targetHostName: 'https://gall.dcinside.com',
 		targetUrl: (page: number) => `https://gall.dcinside.com/board/lists/?id=dcbest&list_num=100&sort_type=N&search_head=9&page=${page}`,
 		page: [1, 1],
 		range: [1, 101],
 		rangeGap: 1,
+		garbage: (idx: number) => [
+			`#container > section.left_content > article:nth-child(3) > div.gall_listwrap.list > table > tbody > tr:nth-child(${idx}) > td.gall_tit.ub-word > a:nth-child(1) > strong`,
+		],
 		targetIndex: (idx: number) =>
 			`#container > section.left_content > article:nth-child(3) > div.gall_listwrap.list > table > tbody > tr:nth-child(${idx}) > td.gall_num`,
 		link: (idx: number) =>
