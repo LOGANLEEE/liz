884c12c662f73566b01e95840259f0349e2709e5
LOGANLEEE <logan_lee@kakao.com>
2022-11-24 23:53:04 +0000

    split axios instance

---
+++ lib/axiosInstance.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export const _axios = axios.create({
+	validateStatus: function (status) {
+		return status < 400; // Resolve only if the status code is less than 500
+	},
+});
+
+// _axios.interceptors.response.use(
+// 	(config) => {
+// 		return config;
+// 	},
+// 	(error) => {
+// 		return Promise.reject(error);
+// 	}
+// );
