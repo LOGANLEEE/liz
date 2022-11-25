ccc8ca9c2aefef4be7427dbe580088ff95586dfa
LOGANLEEE <logan_lee@kakao.com>
2022-11-25 18:22:24 +0000

    add Wrapper

--- containers/PostContainer/index.tsx
+++ containers/PostContainer/index.tsx
@@ -1,15 +1,20 @@
 import { fresh_post } from '@prisma/client';
 import Post from 'components/Post';
+import styled from 'styled-components';
 
 declare type Props = {
 	posts: fresh_post[];
 };
 export const PostContainer = ({ posts }: Props) => {
 	return (
-		<div>
+		<Wrapper>
 			{posts.map((data) => (
 				<Post key={data.id} data={data} />
-			))}{' '}
-		</div>
+			))}
+		</Wrapper>
 	);
 };
+
+const Wrapper = styled.div`
+	width: 100%;
+`;
