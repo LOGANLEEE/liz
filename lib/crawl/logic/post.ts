9443d889873d952c4553ff0d59a79a650fa260ba
LOGANLEEE <logan_lee@kakao.com>
2022-11-26 17:52:27 +0000

    disable where condtion desc

--- lib/crawl/logic/post.ts
+++ lib/crawl/logic/post.ts
@@ -1,6 +1,10 @@
 import { _prisma } from 'prisma/prismaInstance';
 
 export const getFreshPost = async () => {
-	const list = await _prisma.fresh_post.findMany({ where: { mark: false }, orderBy: { hit: 'desc' } });
+	const list = await _prisma.fresh_post.findMany({
+		where: { mark: false },
+		//  orderBy: { hit: 'desc' }
+	});
+
 	return list;
 };
