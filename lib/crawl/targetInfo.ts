060758ef4dc0d99ff5b44651726fd3e7c1e3fd0a
LOGANLEEE <logan_lee@kakao.com>
2022-11-26 17:03:58 +0000

    update page limit

--- lib/crawl/targetInfo.ts
+++ lib/crawl/targetInfo.ts
@@ -5,7 +5,7 @@ export const targetInfo = [
 		name: names.dc,
 		targetHostName: 'https://gall.dcinside.com',
 		targetUrl: (page: number) => `https://gall.dcinside.com/board/lists/?id=dcbest&list_num=100&sort_type=N&search_head=9&page=${page}`,
-		page: [1, 1],
+		page: [1, 2],
 		range: [1, 101],
 		rangeGap: 1,
 		garbage: (idx: number) => [
