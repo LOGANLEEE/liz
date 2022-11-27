cf0537816eda9b0964c33b4931dd1c76a4e9f366
LOGANLEEE <logan_lee@kakao.com>
2022-11-27 00:20:44 +0000

    fix axios instance

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,16 +1,42 @@
 import axios from 'axios';
+import { decode } from 'iconv-lite';
 
 export const _axios = axios.create({
-	validateStatus: function (status) {
-		return status < 400; // Resolve only if the status code is less than 500
+	headers: {
+		'User-Agent':
+			'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+		'Accept-Encoding': 'gzip, deflate, br',
 	},
+
+	// responseType: 'arraybuffer',
+	// responseEncoding: 'binary',
+	responseEncoding: 'utf8',
+	// validateStatus: function (status) {
+	// 	return status < 400; // Resolve only if the status code is less than 500
+	// },
 });
 
-// _axios.interceptors.response.use(
-// 	(config) => {
-// 		return config;
-// 	},
-// 	(error) => {
-// 		return Promise.reject(error);
-// 	}
-// );
+_axios.interceptors.request.use(
+	(req) => {
+		// console.log('req:', req);
+
+		return req;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
+);
+
+_axios.interceptors.response.use(
+	(res) => {
+		const ctype = res.headers['content-type'];
+
+		if (ctype?.includes('charset=euc-kr')) {
+			res.data = decode(Buffer.from(res.data), 'euc-kr');
+		}
+		return res;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
+);
