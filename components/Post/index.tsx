86e9cf9b05b48f6ac998919f6d0da27fee77e65f
LOGANLEEE <logan_lee@kakao.com>
2022-11-30 12:05:20 +0000

    update useEffect and visitHandler
    update css

--- components/Post/index.tsx
+++ components/Post/index.tsx
@@ -1,6 +1,7 @@
 import type { fresh_post } from '@prisma/client';
 import { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { VisitedList } from 'types';
 import { numberFormat } from '../../lib/util';
 
 type Props = {
@@ -8,28 +9,32 @@ type Props = {
 };
 const Post = ({ data }: Props) => {
 	const [visited, setVisited] = useState(false);
+
 	useEffect(() => {
-		const idList: number[] = JSON.parse(localStorage.getItem('visitedIdList') || '[]');
-		if (idList.length < 1) return;
-		if (idList.find((id) => id === data.id) || 0 > 0) setVisited(true);
-	}, [data.id]);
+		const visitedList: VisitedList = JSON.parse(localStorage.getItem('visitedIdList') || '[]');
+		if (visitedList.length < 1) return;
+		const target = visitedList.find(({ id, title, name }) => id === data.id || (title === data.title && name === data.name));
+		if (target?.id || target?.title) {
+			setVisited(true);
+		}
+	}, [data.id, data.name, data.title]);
 
 	const visitHandler = useCallback(() => {
 		if (data.link) window.open(data.link);
-		const idxList: number[] = JSON.parse(localStorage.getItem('visitedIdList') || '[]');
-		idxList.push(data?.id);
-		localStorage.setItem('visitedIdList', JSON.stringify(idxList));
 		setVisited(true);
-
+		const visitedList: VisitedList = JSON.parse(localStorage.getItem('visitedIdList') || '[]');
+		visitedList.push({ id: data?.id, title: data?.title || undefined, name: data.name + '' });
+		localStorage.setItem('visitedIdList', JSON.stringify(visitedList));
 		return;
-	}, [data?.id, data.link]);
+	}, [data?.id, data.link, data.name, data?.title]);
 
 	return (
-		<Wrapper onClick={visitHandler}>
-			<li className={`title ${visited ? 'visited' : ''}`}>
+		<Wrapper onClick={visitHandler} className={`title ${visited ? 'visited' : ''}`}>
+			<li className='title'>
 				<span className='name'>{data.name}</span>
 				{data.title}
 			</li>
+			{/* {data?.author && <li className='author'>{data.author}</li>} */}
 			{data.hit && <li className='hit'>🔥{numberFormat(data.hit)}</li>}
 		</Wrapper>
 	);
@@ -46,20 +51,29 @@ const Wrapper = styled.ul`
 	border-radius: 14px;
 
 	:hover {
+		background-color: #4e2323;
+	}
+	:active {
 		background-color: #602b2b;
 	}
-
+	&.visited {
+		color: #645959 !important;
+	}
 	.title {
-		&.visited {
-			color: #645959;
-		}
-
 		display: flex;
 		flex-direction: row;
 		gap: 10px;
 		.name {
 			flex: auto;
+			border-right: 0.5px solid;
+			padding-right: 8px;
 		}
+	}
+
+	.author {
+		font-size: 10px;
+		text-align: end;
+		flex: auto;
 	}
 	.hit {
 		text-align: end;
