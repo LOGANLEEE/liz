ee54a9eff7f26962a1b14262c4ce81a521fc6d9e
LOGANLEEE <logan_lee@kakao.com>
2022-11-30 12:08:20 +0000

    add disconnecting log

--- prisma/prismaInstance.ts
+++ prisma/prismaInstance.ts
@@ -1,3 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
 export const _prisma = new PrismaClient();
+
+_prisma.$on('beforeExit', async () => {
+	console.log('prisma connection this connected ');
+});
