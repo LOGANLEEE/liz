13e70277a5f4f8f494a96b9b2ebf60809bd86a83
LOGANLEEE <logan_lee@kakao.com>
2022-12-01 09:39:53 +0000

    imple BottomPagination

---
+++ components/BottomPagination/index.tsx
@@ -0,0 +1,26 @@
+import { Grid, Pagination } from '@nextui-org/react';
+import { memo } from 'react';
+import styled from 'styled-components';
+
+type BottomPaginationProps = {
+	totalCount: number;
+	limit: number;
+	page: number;
+	onChangeHandler: (pageNum: number) => void;
+};
+
+export const BottomPagination = memo(({ totalCount, limit, page, onChangeHandler }: BottomPaginationProps) => {
+	return (
+		<Wrapper>
+			<Grid.Container gap={2}>
+				<Grid xs={12}>
+					<Pagination onChange={onChangeHandler} color={'secondary'} total={Math.ceil(totalCount / limit)} page={page} />
+				</Grid>
+			</Grid.Container>
+		</Wrapper>
+	);
+});
+
+BottomPagination.displayName = 'BottomPagination';
+
+const Wrapper = styled(Grid)``;
