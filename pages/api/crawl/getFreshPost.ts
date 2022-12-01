aae5a3f056baaff4f7f27791cf7ec73824dd21a1
LOGANLEEE <logan_lee@kakao.com>
2022-12-01 09:41:58 +0000

    add api

---
+++ pages/api/crawl/getFreshPost.ts
@@ -0,0 +1,22 @@
+import { getFreshPost } from 'lib/crawl/logic/post';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { _prisma } from 'prisma/prismaInstance';
+
+const getFreshPostHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+	if (req.method !== 'POST') {
+		res.status(401).json({ message: 'wrong http method' });
+		return;
+	}
+
+	const { offset, limit } = req.body;
+	// console.log('limit:', limit);
+	// console.log('offset:', offset);
+
+	await _prisma.$connect();
+	const data = await getFreshPost({ limit, offset });
+	res.status(200).json(data);
+	await _prisma.$disconnect();
+	return;
+};
+
+export default getFreshPostHandler;
