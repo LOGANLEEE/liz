aa81b7dea4dbf767b406352927e6eee3d6a1a8d7
LOGANLEEE <logan_lee@kakao.com>
2022-12-02 13:34:29 +0000

    add comp

---
+++ components/CustomLoading/index.tsx
@@ -0,0 +1,15 @@
+import { Loading } from '@nextui-org/react';
+import { memo } from 'react';
+import styled from 'styled-components';
+
+export const CustomLoading = memo(() => {
+	return <StyledLoading type='spinner' size='md' />;
+});
+
+CustomLoading.displayName = 'CustomLoading';
+
+const StyledLoading = styled(Loading)`
+	position: absolute;
+	top: 0;
+	right: 0;
+`;
