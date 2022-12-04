a6d9b46d07567f7025402c7055b0aa42ac9e2132
LOGANLEEE <logan_lee@kakao.com>
2022-12-04 00:11:25 +0000

    add api

--- lib/log.ts
+++ lib/log.ts
@@ -1,3 +1,4 @@
+import type { api_log } from '@prisma/client';
 import { _prisma } from 'prisma/prismaInstance';
 
 type WriteLogArgs = {
@@ -9,4 +10,9 @@ type WriteLogArgs = {
 export const writeLog = async (args: WriteLogArgs) => {
 	await _prisma.api_log.create({ data: args });
 	return;
+};
+
+export const getRecentAccessLog = async (): Promise<api_log | null> => {
+	const recentAccessLog = await _prisma.api_log.findFirst({ where: { name: 'accessor' }, orderBy: { create_date: 'desc' } });
+	return recentAccessLog;
 };
