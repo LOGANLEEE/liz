2f81cde5bf29fe2bfdd7d15366b9d0b2620ff58c
LOGANLEEE <logan_lee@kakao.com>
2022-12-04 17:22:43 +0000

    mod test

--- pages/api/crawl/test.ts
+++ pages/api/crawl/test.ts
@@ -1,10 +1,10 @@
-import { FMKOREAaccessor } from 'lib/crawl/logic/accessor/fmkorea';
+import { RULIWEBAccessor } from 'lib/crawl/logic/accessor/ruliweb';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { _prisma } from 'prisma/prismaInstance';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	await _prisma.$connect();
-	const result = await FMKOREAaccessor();
+	const result = await RULIWEBAccessor();
 	res.status(200).json({ ...result });
 	await _prisma.$disconnect();
 };
