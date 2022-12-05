67fecbf8f44f93aadc806e4ce97b9ab52d691e32
LOGANLEEE <logan_lee@kakao.com>
2022-12-05 00:07:46 +0000

    update test code

--- pages/api/crawl/test.ts
+++ pages/api/crawl/test.ts
@@ -1,10 +1,18 @@
+import { DCINSIDEAccessor } from 'lib/crawl/logic/accessor/dcinside';
+import { FMKOREAaccessor } from 'lib/crawl/logic/accessor/fmkorea';
 import { RULIWEBAccessor } from 'lib/crawl/logic/accessor/ruliweb';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { _prisma } from 'prisma/prismaInstance';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+	console.log('===>crawling tester started');
+
 	await _prisma.$connect();
-	const result = await RULIWEBAccessor();
+	// const result = await DCINSIDEAccessor();
+	// const result = await RULIWEBAccessor();
+	const result = await FMKOREAaccessor();
+
+	console.log('====>crawling tester finished');
 	res.status(200).json({ ...result });
 	await _prisma.$disconnect();
 };
