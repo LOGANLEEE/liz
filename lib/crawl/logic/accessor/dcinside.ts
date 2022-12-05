a9ed2031d72843b2ed567360b732aeede66cd448
LOGANLEEE <logan_lee@kakao.com>
2022-12-05 00:06:36 +0000

    update jsdom + axios to puppeteer

--- lib/crawl/logic/accessor/dcinside.ts
+++ lib/crawl/logic/accessor/dcinside.ts
@@ -1,49 +1,75 @@
-import { JSDOM } from 'jsdom';
-import { _axios } from 'lib/axiosInstance';
+import { writeFile } from 'fs/promises';
 import { DCINSIDE_INFO } from 'lib/crawl/targetInfo';
 import { delay, masking } from 'lib/util';
 import { _prisma } from 'prisma/prismaInstance';
+import puppeteer from 'puppeteer';
 
 export const DCINSIDEAccessor = async (): Promise<{ count: number; isError: boolean; name: string; message: string }> => {
 	// get target Info
-	const { author, hit, link, name, page, range, rangeGap, targetUrl, targetIndex, garbage, targetHostName } = DCINSIDE_INFO;
 
+	const browser = await puppeteer.launch({ headless: true });
+
+	const page = await browser.newPage();
+	await page.setUserAgent(
+		'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+	);
 	const tempHolder = [];
 
-	for (let pageCount = page[0]; pageCount <= page[1]; pageCount += 1) {
+	for (let pageCount = DCINSIDE_INFO.pageRange[0]; pageCount <= DCINSIDE_INFO.pageRange[1]; pageCount += DCINSIDE_INFO.pageRange[2]) {
+		await page.goto(DCINSIDE_INFO.targetUrl(pageCount));
 		await delay(pageCount * 500);
-		const { data, error } = await _axios.get(targetUrl(pageCount)).catch((error) => error);
 
-		if (!error && data) {
-			const fragment = JSDOM.fragment(data);
-			for (let idx = range[0]; idx <= range[1]; idx += rangeGap) {
-				// remove garbage tag
-				garbage(idx).forEach((target) => {
-					fragment.querySelector(target)?.remove();
-				});
+		// await writeFile('./dummy.html', await page.content()).then(() => console.log('file wrote'));
 
-				const postIndex = fragment.querySelector(targetIndex(idx))?.textContent || 'null';
+		for (let postCount = DCINSIDE_INFO.postRange[0]; postCount <= DCINSIDE_INFO.postRange[1]; postCount += DCINSIDE_INFO.postRange[2]) {
+			const postIndex =
+				(await page
+					.waitForSelector(DCINSIDE_INFO.targetIndex(postCount))
+					.then((element) => element?.evaluate((el) => el?.textContent?.trim()))) || '';
 
-				if (isNaN(parseInt(postIndex))) continue;
+			if (isNaN(parseInt(postIndex))) continue;
 
-				const _title = fragment.querySelector(link(idx))?.textContent || masking;
-				if (_title === masking) return { count: 0, isError: true, name, message: 'empty title' };
+			// remove garbage tag
+			DCINSIDE_INFO.garbage(postCount).forEach(
+				async (path) => await page.waitForSelector(path).then((element) => element?.evaluate((el) => el.remove()))
+			);
 
-				const _link = fragment.querySelector(link(idx))?.getAttribute('href') || masking;
-				if (_link === masking) return { count: 0, isError: true, name, message: 'empty link' };
+			const title =
+				(await page
+					.waitForSelector(DCINSIDE_INFO.link(postCount))
+					.then((element) => element?.evaluate((el) => el?.textContent?.trim()))) || masking;
+			if (title === masking) return { count: 0, isError: true, name: DCINSIDE_INFO.name, message: 'empty title' };
 
-				const _hit = parseInt(fragment.querySelector(hit(idx))?.textContent || '---');
-				if (isNaN(_hit)) return { count: 0, isError: true, name, message: 'empty hit' };
+			const link =
+				(await page
+					.waitForSelector(DCINSIDE_INFO.link(postCount))
+					.then((element) => element?.evaluate((el) => el?.getAttribute('href')))) || masking;
+			if (link === masking) return { count: 0, isError: true, name: DCINSIDE_INFO.name, message: 'empty link' };
 
-				const _author = fragment.querySelector(author(idx))?.getAttribute('title') || masking;
-				if (_author === masking) return { count: 0, isError: true, name, message: 'empty author' };
+			const hit = parseInt(
+				(await page.waitForSelector(DCINSIDE_INFO.hit(postCount)).then((element) => element?.evaluate((el) => el?.textContent))) ||
+					'---'
+			);
+			if (isNaN(hit)) return { count: 0, isError: true, name: DCINSIDE_INFO.name, message: 'empty hit' };
 
-				tempHolder.push({ link: `${targetHostName}${_link}`, author: _author, hit: _hit, name, title: _title, mark: false });
-			}
+			const author =
+				(await page
+					.waitForSelector(DCINSIDE_INFO.author(postCount))
+					?.then((element) => element?.evaluate((el) => el?.getAttribute('title')))) || masking;
+			if (author === masking) return { count: 0, isError: true, name: DCINSIDE_INFO.name, message: 'empty author' };
+
+			tempHolder.push({
+				link: `${DCINSIDE_INFO.targetHostName}${link}`,
+				author,
+				hit,
+				name: DCINSIDE_INFO.name,
+				title: title,
+				mark: false,
+			});
 		}
 	}
-	if (tempHolder.length < 1) return { count: 0, isError: true, name, message: 'tempHolder is empty' };
+	if (tempHolder.length < 1) return { count: 0, isError: true, name: DCINSIDE_INFO.name, message: 'tempHolder is empty' };
 
 	const { count } = await _prisma.fresh_post.createMany({ data: tempHolder });
-	return { count, isError: false, message: '', name };
+	return { count, isError: false, message: '', name: DCINSIDE_INFO.name };
 };
