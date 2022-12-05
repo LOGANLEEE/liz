1dd3101da794588b6c1d654852d596f55a92ed1e
LOGANLEEE <logan_lee@kakao.com>
2022-12-05 22:38:39 +0000

    add openAll post func

--- containers/PostContainer/index.tsx
+++ containers/PostContainer/index.tsx
@@ -1,14 +1,42 @@
-import { fresh_post } from '@prisma/client';
+import { Button } from '@nextui-org/react';
+import type { fresh_post } from '@prisma/client';
 import Post from 'components/Post';
-import { memo } from 'react';
+import { memo, useCallback, useEffect, useState } from 'react';
+import { isSafari } from 'react-device-detect';
 import styled from 'styled-components';
+import { VisitedList } from 'types';
 
 declare type Props = {
 	posts?: fresh_post[];
 };
 export const PostContainer = memo(({ posts }: Props) => {
+	const [showButton, setShowButton] = useState(false);
+
+	useEffect(() => {
+		if (isSafari) {
+			setShowButton(true);
+		}
+	}, []);
+
+	const openPosts = useCallback(() => {
+		if (showButton) {
+			const visitedList: VisitedList = JSON.parse(localStorage.getItem('visitedIdList') || '[]');
+			posts?.forEach(({ id, title, name, link }) => {
+				link && window.open(link, '');
+				return visitedList.push({ id, title: title || undefined, name: name || '' });
+			});
+
+			localStorage.setItem('visitedIdList', JSON.stringify(visitedList));
+		}
+	}, [posts, showButton]);
+
 	return (
 		<Wrapper>
+			{showButton && (
+				<Button shadow rounded bordered onClick={openPosts} size={'xs'}>
+					모두 보기
+				</Button>
+			)}
 			{posts?.map((data) => (
 				<Post key={data.id} data={data} />
 			))}
