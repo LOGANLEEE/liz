5236a06e3bcc3d9d8de0d057db96212e47bdabb8
LOGANLEEE <logan_lee@kakao.com>
2022-12-06 12:22:02 +0000

    add puppeteerUserAgent

--- lib/util.ts
+++ lib/util.ts
@@ -15,3 +15,6 @@ export const numberFormat = (num: number) => numberFormatter.format(num);
 export const clientTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 export const puppeteerArgs = ['--disable-gpu', '--disable-dev-shm-usage', '--no-sandbox', '--disabled-setupid-sandbox'];
+
+export const puppeteerUserAgent =
+	'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36';
