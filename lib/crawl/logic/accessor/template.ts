91840e3077ceb28e048bd21292a3e80ea8903518
LOGANLEEE <logan_lee@kakao.com>
2022-12-07 22:37:27 +0000

    update template

--- lib/crawl/logic/accessor/template.ts
+++ lib/crawl/logic/accessor/template.ts
@@ -1,4 +1,5 @@
-import { ILBE_INFO } from 'lib/crawl/targetInfo';
+import { writeFile } from 'fs/promises';
+import { INVEN_INFO } from 'lib/crawl/targetInfo';
 import { puppeteerUserAgent } from 'lib/util';
 import { _prisma } from 'prisma/prismaInstance';
 import { Browser } from 'puppeteer';
@@ -15,57 +16,69 @@ export const accessorTemplate = async ({
 	const page = await browser.newPage();
 	await page.setUserAgent(puppeteerUserAgent);
 
-	for (let pageCount = ILBE_INFO.pageRange[0]; pageCount <= ILBE_INFO.pageRange[1]; pageCount += ILBE_INFO.pageRange[2]) {
+	for (let pageCount = INVEN_INFO.pageRange[0]; pageCount <= INVEN_INFO.pageRange[1]; pageCount += INVEN_INFO.pageRange[2]) {
+		console.log(`===> ${INVEN_INFO.name} ${pageCount} / ${INVEN_INFO.pageRange[1]}`);
 		const tempHolder = [];
-		await page.goto(ILBE_INFO.targetUrl(pageCount));
+		await page.goto(INVEN_INFO.targetUrl(pageCount));
+		if (pageCount === 1) {
+			await writeFile('./dummy.html', await page.content()).then(() => {
+				console.log('html write.');
+			});
+			await writeFile('./dummy.png', await page.screenshot()).then(() => {
+				console.log('png write.');
+			});
+		}
 
-		for (let postCount = ILBE_INFO.postRange[0]; postCount <= ILBE_INFO.postRange[1]; postCount += ILBE_INFO.postRange[2]) {
-			const title =
-				(await page
-					.waitForSelector(ILBE_INFO.link(postCount))
-					.then((element) => element?.evaluate((el) => el.textContent?.trim()))
-					.catch((err) => {
-						console.log(err);
-						return 'error title';
-					})) || null;
-			const link =
-				(await page
-					.waitForSelector(ILBE_INFO.link(postCount))
-					.then((element) => element?.evaluate((el) => el.getAttribute('href')))
-					.catch((err) => {
-						console.log(err);
-						return 'error link';
-					})) || null;
+		for (let postCount = INVEN_INFO.postRange[0]; postCount <= INVEN_INFO.postRange[1]; postCount += INVEN_INFO.postRange[2]) {
+			if (INVEN_INFO.garbage) {
+				INVEN_INFO.garbage(postCount).forEach(
+					async (path) =>
+						await page.waitForSelector(path).then((element) => element?.evaluate((el) => el.remove()).catch(() => {}))
+				);
+			}
 
-			const author =
-				(await page
-					.waitForSelector(ILBE_INFO.author(postCount))
-					.then((element) => element?.evaluate((el) => el.textContent?.trim()))
-					.catch((err) => {
-						console.log(err);
-						return 'error author';
-					})) || null;
+			const title = await page
+				.waitForSelector(INVEN_INFO?.title ? INVEN_INFO?.title(postCount) : INVEN_INFO.link(postCount))
+				.then((element) => element?.evaluate((el) => el.textContent?.trim()))
+				.catch(() => null);
 
-			const hit =
-				(await page
-					.waitForSelector(ILBE_INFO.hit(postCount))
-					.then((element) => element?.evaluate((el) => parseInt(el.textContent?.trim()?.replaceAll(',', '') || '0')))
-					.catch((err) => {
-						console.log(err);
-						return -1;
-					})) || null;
+			const link = await page
+				.waitForSelector(INVEN_INFO.link(postCount))
+				.then((element) => element?.evaluate((el) => el.getAttribute('href')))
+				.catch(() => null);
+
+			const author = await page
+				.waitForSelector(INVEN_INFO.author(postCount))
+				.then((element) => element?.evaluate((el) => el.textContent?.trim()))
+				.catch(() => '::author::');
+
+			const hit = await page
+				.waitForSelector(INVEN_INFO.hit(postCount))
+				.then((element) =>
+					element?.evaluate((el) =>
+						parseInt(
+							el.textContent?.trim()?.replaceAll(' ', '')?.replaceAll(',', '')?.replaceAll('.', '')?.replaceAll('k', '00') ||
+								'-1'
+						)
+					)
+				)
+				.catch(() => -1);
 
 			if (!title && !link) continue;
 
-			tempHolder.push({ title, link, hit, name: ILBE_INFO.name, mark: false, author, content: null });
+			tempHolder.push({
+				title,
+				link: `${INVEN_INFO.targetBaseName}${link}`,
+				hit,
+				name: INVEN_INFO.name,
+				mark: false,
+				author,
+				content: null,
+			});
 		}
 		const { count } = await _prisma.fresh_post.createMany({ data: tempHolder });
 		totalCount += count;
 	}
 	await page.close();
-	return { count: totalCount, isError: false, message: 'good', name: ILBE_INFO.name };
+	return { count: totalCount, isError: false, message: 'good', name: INVEN_INFO.name };
 };
-
-// await writeFile('./dummy.html', await newPage.content()).then(() => {
-// 	console.log('file write.');
-// });
