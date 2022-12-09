48430c2d528c5d359d7b160abfee7ab9564f8b4f
LOGANLEEE <logan_lee@kakao.com>
2022-12-09 00:11:25 +0000

    lint

--- containers/InfoBar/index.tsx
+++ containers/InfoBar/index.tsx
@@ -1,4 +1,4 @@
-import { Card, Grid, Spacer, Text } from '@nextui-org/react';
+import { Grid, Spacer, Text } from '@nextui-org/react';
 import { memo } from 'react';
 import styled from 'styled-components';
 
