a1718abc98087af1a8cb1f5dc4e27ee92c376b7b
LOGANLEEE <logan_lee@kakao.com>
2022-12-09 00:13:00 +0000

    add global value

--- lib/util.ts
+++ lib/util.ts
@@ -18,3 +18,7 @@ export const puppeteerArgs = ['--disable-gpu', '--disable-dev-shm-usage', '--no-
 
 export const puppeteerUserAgent =
 	'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36';
+
+export const stateTest = {
+	isCrawling: false,
+};
