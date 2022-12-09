e1a45df156758fb94872c578695d725d000159b3
LOGANLEEE <logan_lee@kakao.com>
2022-12-09 11:38:22 +0000

    lint

--- containers/Layout/index.tsx
+++ containers/Layout/index.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from 'react';
 import styled from 'styled-components';
-import { TopNavigationBar } from '../../components/NavBar';
+import { TopNavigationBar } from '../../components/TopNavigationBar';
 
 export const Layout = ({ children }: { children: ReactNode }) => {
 	return (
