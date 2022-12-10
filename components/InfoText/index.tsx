2892ab02df171bab47801a4575bf075c7a333526
LOGANLEEE <logan_lee@kakao.com>
2022-12-10 11:57:13 +0000

    add infoText remove infoBox

---
+++ components/InfoText/index.tsx
@@ -0,0 +1,54 @@
+import { Grid, Spacer, Text } from '@nextui-org/react';
+import type { api_log } from '@prisma/client';
+import { format } from 'date-fns';
+import { memo } from 'react';
+import styled from 'styled-components';
+
+type Props = {
+	postCount: number;
+	targetSiteCount: number;
+	recentAccessLog?: api_log;
+};
+export const InfoText = memo(({ postCount, targetSiteCount, recentAccessLog }: Props) => {
+	return (
+		<Wrapper justify='flex-start' gap={0.5} direction='column'>
+			{recentAccessLog?.create_date && (
+				<Grid xs sm md lg xl>
+					<Text>마지막으로 </Text>
+					<Spacer x={0.2} />
+					<Text color='primary'>{format(new Date(recentAccessLog?.create_date), `MM월 dd일  HH시mm분ss초`)}</Text>
+					<Spacer x={0.2} />
+					<Text> 에 동작했어요.</Text>
+				</Grid>
+			)}
+			{!recentAccessLog?.create_date && (
+				<Grid xs sm md lg xl>
+					<Text>매 </Text>
+					<Spacer x={0.2} />
+					<Text color='primary'>30분</Text>
+					<Spacer x={0.2} />
+					<Text>마다 동작할 예정이에요.</Text>
+				</Grid>
+			)}
+			<Grid xs sm md lg xl>
+				<Text>총</Text>
+				<Spacer x={0.2} />
+				<Text color='primary'>{targetSiteCount}</Text>
+				<Spacer x={0.2} />
+				<Text>군데에서</Text>
+				<Spacer x={0.2} />
+				<Text color='primary'>{postCount}</Text>
+				<Spacer x={0.2} />
+				<Text>개의 게시물을 긁어왔어요!</Text>
+			</Grid>
+		</Wrapper>
+	);
+});
+
+InfoText.displayName = 'InfoBar';
+
+const Wrapper = styled(Grid.Container)`
+	background-color: #193631;
+	border-radius: 10px;
+	/* width: 100%; */
+`;
