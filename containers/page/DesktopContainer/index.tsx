f9996ca724a785a9df428300e03c8f1510defb11
LOGANLEEE <logan_lee@kakao.com>
2022-12-10 11:57:33 +0000

    rename page container

--- containers/page/DesktopContainer/index.tsx
+++ containers/page/DesktopContainer/index.tsx
@@ -1,18 +1,17 @@
 import { Grid } from '@nextui-org/react';
 import type { api_log, fresh_post } from '@prisma/client';
 import { BottomPagination } from 'components/BottomPagination';
-import { InfoCard } from 'containers/InfoBar';
+import { InfoText } from 'components/InfoText';
 import { PostContainer } from 'containers/PostContainer';
-import { format } from 'date-fns';
 
 type Props = {
 	totalCount: number;
 	targetSiteCount: number;
 	limit: number;
-	recentAccessLog: api_log;
 	pageIdx: number;
 	pageIdxHandler: (pageNum: number) => void;
 	freshPostList: fresh_post[];
+	recentAccessLog?: api_log;
 };
 
 const DesktopContainer = ({ limit, pageIdxHandler, pageIdx, targetSiteCount, totalCount, freshPostList, recentAccessLog }: Props) => {
@@ -24,21 +23,7 @@ const DesktopContainer = ({ limit, pageIdxHandler, pageIdx, targetSiteCount, tot
 			<Grid xs={10} sm={8} md={10} lg={10} xl={10}>
 				<Grid.Container justify='center' direction='row' gap={2}>
 					<Grid xs={12} sm={12} md={12} lg={12} xl={12} justify='center'>
-						{/* <InfoBar postCount={totalCount} targetSiteCount={Object.keys(names).length} /> */}
-						<Grid.Container justify='flex-start' gap={1}>
-							<Grid xs sm md lg xl>
-								<InfoCard
-									count={totalCount}
-									title='최근 동작 시점'
-									titleInfo={format(new Date(recentAccessLog.create_date), `MM월 dd일  HH시mm분ss초`)}
-									prefix='총'
-									suffix='개'
-								/>
-							</Grid>
-							<Grid xs sm md lg xl>
-								<InfoCard count={targetSiteCount} titleInfo='대상 커뮤니티' suffix='군데' />
-							</Grid>
-						</Grid.Container>
+						<InfoText targetSiteCount={targetSiteCount} postCount={totalCount} recentAccessLog={recentAccessLog} />
 					</Grid>
 					<Grid xs={12} sm={12} md={12} lg={12} xl={12} justify='center'>
 						<BottomPagination limit={limit} totalCount={totalCount} page={pageIdx} onChangeHandler={pageIdxHandler} />
--- containers/page/MobileContainer/index.tsx
+++ containers/page/MobileContainer/index.tsx
@@ -1,9 +1,8 @@
 import { Grid } from '@nextui-org/react';
 import type { api_log, fresh_post } from '@prisma/client';
 import { BottomPagination } from 'components/BottomPagination';
-import { InfoCard } from 'containers/InfoBar';
+import { InfoText } from 'components/InfoText';
 import { PostContainer } from 'containers/PostContainer';
-import { format } from 'date-fns';
 import styled from 'styled-components';
 
 type Props = {
@@ -13,28 +12,14 @@ type Props = {
 	pageIdx: number;
 	pageIdxHandler: (pageNum: number) => void;
 	freshPostList: fresh_post[];
-	recentAccessLog: api_log;
+	recentAccessLog?: api_log;
 };
 
 const MobileContainer = ({ limit, pageIdxHandler, pageIdx, recentAccessLog, targetSiteCount, totalCount, freshPostList }: Props) => {
 	return (
 		<Wrapper direction='row' justify='center' gap={1}>
 			<Grid xs={11.5} sm={11.5} md={11.5} lg={11.5} xl={11.5} justify='center'>
-				{/* <InfoBar postCount={totalCount} targetSiteCount={Object.keys(names).length} /> */}
-				<Grid.Container justify='center' gap={1}>
-					<Grid xs sm>
-						<InfoCard
-							count={totalCount}
-							title='최근 동작 시점'
-							titleInfo={format(new Date(recentAccessLog.create_date), `MM월 dd일  HH시 mm분 ss초`)}
-							prefix='총'
-							suffix='개'
-						/>
-					</Grid>
-					<Grid xs sm>
-						<InfoCard count={targetSiteCount} titleInfo='대상 커뮤니티' suffix='군데' />
-					</Grid>
-				</Grid.Container>
+				<InfoText targetSiteCount={targetSiteCount} postCount={totalCount} recentAccessLog={recentAccessLog} />
 			</Grid>
 			<Grid xs={11.5} sm={11.5} md={11.5} lg={11.5} xl={11.5} justify='center'>
 				<BottomPagination limit={limit} totalCount={totalCount} page={pageIdx} onChangeHandler={pageIdxHandler} />
