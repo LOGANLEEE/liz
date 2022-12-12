c88d631e3a1d82868e4673d633d86000564a6bf2
LOGANLEEE <logan_lee@kakao.com>
2022-12-12 22:20:08 +0000

    imprv logic

--- lib/crawl/logic/cleaner.ts
+++ lib/crawl/logic/cleaner.ts
@@ -1,14 +1,14 @@
 import { _prisma } from 'prisma/prismaInstance';
 
-export const markingFreshPosts = async () => {
-	const { count } = await _prisma.fresh_post.updateMany({ data: { mark: true }, where: { mark: false } });
-	return { count };
-};
+// export const stagePreparation = async () => {
+// 	const { count } = await _prisma.fresh_post.updateMany({ data: { mark: false }, where: { mark: false } });
+// 	return { count };
+// };
 
-export const moveMarkedPosts = async () => {
+export const afterStageCleanUp = async () => {
 	const freshes = await _prisma.fresh_post.findMany({ where: { mark: true } });
 	const { count: movedCount } = await _prisma.old_post.createMany({ data: freshes });
 	const { count: deletedCount } = await _prisma.fresh_post.deleteMany({ where: { mark: true } });
-
-	return { movedCount, deletedCount };
+	const { count: updatedCount } = await _prisma.fresh_post.updateMany({ data: { mark: true }, where: { mark: false } });
+	return { movedCount, deletedCount, updatedCount };
 };
