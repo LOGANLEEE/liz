4c9d7add3c9005cb6434f9074edc8e1023463655
LOGANLEEE <logan_lee@kakao.com>
2022-12-12 22:20:34 +0000

    enable order by hit

--- lib/crawl/logic/post.ts
+++ lib/crawl/logic/post.ts
@@ -1,6 +1,8 @@
 import type { fresh_post } from '@prisma/client';
 import { _prisma } from 'prisma/prismaInstance';
 
+export declare type OrderBy = 'desc' | 'asc';
+
 type GetFreshPostArgs = {
 	offset?: number;
 	limit?: number;
@@ -14,9 +16,9 @@ export declare type GetFreshPostReturn = {
 
 export const getFreshPost = async ({ offset, limit, orderByHit }: GetFreshPostArgs): Promise<GetFreshPostReturn> => {
 	const [totalCount, list] = await _prisma.$transaction([
-		_prisma.fresh_post.count({ where: { mark: false } }),
+		_prisma.fresh_post.count({ where: { mark: true } }),
 		_prisma.fresh_post.findMany({
-			// where: { mark: false },
+			where: { mark: true },
 			skip: offset,
 			take: limit,
 			orderBy: { hit: orderByHit || undefined },
