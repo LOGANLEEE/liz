e1d79317646736b1f123a27268d9b225c2252b34
LOGANLEEE <logan_lee@kakao.com>
2022-12-12 22:21:22 +0000

    add api

---
+++ pages/api/crawl/parallel-init.ts
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { parallelRunner } from 'lib/crawl/logic/parallelRunner';
+import { serverState } from 'lib/state';
+import type { NextApiRequest, NextApiResponse } from 'next';
+// const fetcher = (url) => axios.get(url).then((res) => res.data);
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	// res.status(200).json({ message: 'crawling is starting...' });
+	if (serverState.isCrawling) {
+		res.status(200).json({ message: 'parallel crawling is proceeding...' });
+		return;
+	}
+	parallelRunner();
+	res.status(200).json({ message: ' parallelstart crawling...' });
+	return;
+}
