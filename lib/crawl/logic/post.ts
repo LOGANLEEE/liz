4455f52ce2fd11607e1954758e75055375b9abe7
LOGANLEEE <logan_lee@kakao.com>
2022-12-13 23:10:04 +0000

    enable search

--- lib/crawl/logic/post.ts
+++ lib/crawl/logic/post.ts
@@ -7,6 +7,7 @@ type GetFreshPostArgs = {
 	offset?: number;
 	limit?: number;
 	orderByHit?: 'desc' | 'asc';
+	searchText: string;
 };
 
 export declare type GetFreshPostReturn = {
@@ -14,11 +15,11 @@ export declare type GetFreshPostReturn = {
 	list: fresh_post[];
 };
 
-export const getFreshPost = async ({ offset, limit, orderByHit }: GetFreshPostArgs): Promise<GetFreshPostReturn> => {
+export const getFreshPost = async ({ offset, limit, orderByHit, searchText }: GetFreshPostArgs): Promise<GetFreshPostReturn> => {
 	const [totalCount, list] = await _prisma.$transaction([
 		_prisma.fresh_post.count({ where: { mark: true } }),
 		_prisma.fresh_post.findMany({
-			where: { mark: true },
+			where: { mark: true, title: { contains: searchText } },
 			skip: offset,
 			take: limit,
 			orderBy: { hit: orderByHit || undefined },
