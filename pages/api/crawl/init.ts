03cfcca4d02749fc7196ff18b3555a3f9c6e54ab
LOGANLEEE <logan_lee@kakao.com>
2022-12-13 23:09:41 +0000

    enable axios crawling

--- pages/api/crawl/init.ts
+++ pages/api/crawl/init.ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { sequentialRunner } from 'lib/crawl/logic/sequentialRunner';
+import { axiosParallelRunner } from 'lib/crawl/logic/axiosParallelRunner';
 import { serverState } from 'lib/state';
 import type { NextApiRequest, NextApiResponse } from 'next';
 // const fetcher = (url) => axios.get(url).then((res) => res.data);
@@ -7,10 +7,10 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	// res.status(200).json({ message: 'crawling is starting...' });
 	if (serverState.isCrawling) {
-		res.status(200).json({ message: 'sequential crawling is proceeding...' });
+		res.status(200).json({ message: 'axios parallel crawling is proceeding...' });
 		return;
 	}
-	sequentialRunner();
-	res.status(200).json({ message: 'sequential  start crawling...' });
+	axiosParallelRunner();
+	res.status(200).json({ message: 'axios parallel start crawling...' });
 	return;
 }
