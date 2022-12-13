30e984197cd8262afb2816c0c720898b6cef8e57
LOGANLEEE <logan_lee@kakao.com>
2022-12-13 23:09:51 +0000

    add new instance

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,21 +1,9 @@
 import axios from 'axios';
 import { decode } from 'iconv-lite';
 
-export const _axios = axios.create({
-	// headers: {
-	// 	'User-Agent':
-	// 		'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
-	// 	'Accept-Encoding': 'gzip, deflate, br',
-	// },
-	// responseType: 'arraybuffer',
-	// responseEncoding: 'binary',
-	// responseEncoding: 'utf8',
-	// validateStatus: function (status) {
-	// 	return status < 400; // Resolve only if the status code is less than 500
-	// },
-});
+export const _axios = axios.create({});
 
-_axios.interceptors.request.use(
+axios.interceptors.request.use(
 	(req) => {
 		// console.log('req:', req);
 
@@ -26,7 +14,44 @@ _axios.interceptors.request.use(
 	}
 );
 
-_axios.interceptors.response.use(
+axios.interceptors.response.use(
+	(res) => {
+		const ctype = res.headers['content-type'];
+
+		if (ctype?.includes('charset=euc-kr')) res.data = decode(Buffer.from(res.data), 'euc-kr');
+
+		return res;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
+);
+
+export const _axiosCrawler = axios.create({
+	// headers: {
+	// 'User-Agent':
+	// 	'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+	// },
+	responseType: 'arraybuffer',
+	// responseEncoding: 'binary',
+	// responseEncoding: 'utf8',
+	// validateStatus: function (status) {
+	// 	return status < 400; // Resolve only if the status code is less than 500
+	// },
+});
+
+_axiosCrawler.interceptors.request.use(
+	(req) => {
+		// console.log('req:', req);
+
+		return req;
+	},
+	(error) => {
+		return Promise.reject(error);
+	}
+);
+
+_axiosCrawler.interceptors.response.use(
 	(res) => {
 		const ctype = res.headers['content-type'];
 
