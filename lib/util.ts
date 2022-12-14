672b23d127aaef7dc80511d7270faf35cdefc150
LOGANLEEE <logan_lee@kakao.com>
2022-12-14 23:19:01 +0000

    add val

--- lib/util.ts
+++ lib/util.ts
@@ -59,3 +59,5 @@ export const puppeteerUserAgent =
 	'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36';
 
 export const measure = (st: number, et: number, divide: number) => ((et - st) / divide)?.toFixed(3);
+
+export const humorUniCookie = `link=ok; c_cpuid_uuid=c26-f8a8-0e76; c_check=d2acd7807c7b4d43411b2ca03c6b2cfa; c_cpuid=H-68f6-b224; c_cpuid_set=wOLfwOR3TpSlTprV7qjlvtLlZnCGwJDVvJwkTlHo; __utmc=150955945; __utmz=150955945.1671054969.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); c_uuid_global=c26-f8a8-0e76; hu_auto_cook=Fl9; __utma=150955945.387003015.1671054969.1671054969.1671056936.2; __utmb=150955945.0.10.1671056936; adfit_sdk_id=2523df7b-38dd-4279-b3df-ee7d0d3a3f29; cto_bundle=5A9yzl9RWjBPVXhGOFlNbnc4QiUyRiUyRjBxRmxBV1JXc29GZzdpJTJCOHhsVUM0VWQ4NlViJTJGVEpiQ3dsQXFjMTBGZjJzNXFsZllvVlRFTSUyQkIybjMlMkZUc0xSMzVYSVpZVGxjRGglMkZoN0pCWUlFejJzRTExOFdCNlpGdU9OS1c0WjlRYlh6MnEwN2h6V0NpRldwbEtzOW0xWSUyRktyZGhmcnJtSGtJaHBMbGVqZUFZc21Hd1RxN25jNmUlMkZhR014aktFTGVwUDclMkJHWnB1eFc5MzB1T25Fa0paRDVpWUp6MG5BWUElM0QlM0Q; wcs_bt=395c7d0a9352ac:1671057182`;
