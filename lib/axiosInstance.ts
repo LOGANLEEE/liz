51ec18f8576ffecbf1c18d4214f7ce1b23a81639
LOGANLEEE <logan_lee@kakao.com>
2022-12-16 12:16:03 +0000

    add config

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -28,7 +28,7 @@ export const _axiosCrawler = axios.create({
 		'User-Agent':
 			'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
 	},
-	// withCredentials: true,
+	withCredentials: true,
 	responseType: 'arraybuffer',
 	// responseEncoding: 'binary',
 	// responseEncoding: 'utf8',
