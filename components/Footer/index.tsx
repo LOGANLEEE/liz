534c5f357fd5ce54c21ed5a84df585e528502ad6
LOGANLEEE <logan_lee@kakao.com>
2022-12-16 23:43:18 +0000

    add footer

---
+++ components/Footer/index.tsx
@@ -0,0 +1,31 @@
+import { Grid, Text } from '@nextui-org/react';
+import styled from 'styled-components';
+
+export const Footer = () => {
+	return (
+		<Wrapper>
+			<Grid.Container xs sm md lg xl direction='row' justify='center' gap={1} className='container'>
+				<Grid className='item1'>
+					<Text className='copy-right'>Copyright © {new Date().getFullYear()} LOGAN LEE. All rights reserved.</Text>
+				</Grid>
+			</Grid.Container>
+		</Wrapper>
+	);
+};
+
+const Wrapper = styled.footer`
+	height: 100px;
+	display: flex;
+
+	background: linear-gradient(to bottom, #132020, #123029);
+
+	.item1 {
+		margin-top: auto;
+	}
+	.copy-right {
+		text-align: center;
+	}
+	.container {
+		margin-bottom: 0;
+	}
+`;
