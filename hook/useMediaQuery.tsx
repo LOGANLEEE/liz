37efa6eae74b2aa50f0298338fef3057065e080b
LOGANLEEE <logan_lee@kakao.com>
2022-12-16 23:44:18 +0000

    add hook

---
+++ hook/useMediaQuery.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+
+// xs <650
+// sm 650
+// md 960
+// lg 1280
+// xl 1400
+export const useMediaQuery = (width: number) => {
+	const [targetReached, setTargetReached] = React.useState(false);
+
+	const updateTarget = React.useCallback((e: MediaQueryListEvent) => {
+		if (e.matches) {
+			setTargetReached(true);
+		} else {
+			setTargetReached(false);
+		}
+	}, []);
+
+	useEffect(() => {
+		const media = window.matchMedia(`(max-width: ${width}px)`);
+		media.addListener(updateTarget);
+
+		// Check on mount (callback is not called until a change occurs)
+		if (media.matches) {
+			setTargetReached(true);
+		}
+
+		return () => media.removeListener(updateTarget);
+	}, [updateTarget, width]);
+
+	return targetReached;
+};
