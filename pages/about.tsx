e5388542539dc587a51bc926cdd5f310ba397b46
LOGANLEEE <logan_lee@kakao.com>
2022-12-17 14:39:35 +0000

    add about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,13 +1,19 @@
+import { Text } from '@nextui-org/react';
 import { GetServerSidePropsContext } from 'next';
 import styled from 'styled-components';
 
 const About = () => {
-	<Wrapper>about</Wrapper>;
+	<Wrapper>
+		<Text h1>준비 중 입니다.</Text>
+	</Wrapper>;
 };
 
 export default About;
 
-const Wrapper = styled.main``;
+const Wrapper = styled.main`
+	width: 100vw;
+	height: 100vh;
+`;
 
 function getServerSideProps(context: GetServerSidePropsContext) {
 	return {
