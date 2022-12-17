57931bd5c9e8e7e699382a0c20dde06e12e45d60
LOGANLEEE <logan_lee@kakao.com>
2022-12-17 21:59:59 +0000

    add landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,71 +1,18 @@
-import type { api_log, fresh_post } from '@prisma/client';
-import { CustomLoading } from 'components/CustomLoading';
-import { Footer } from 'components/Footer';
-import { usePagination } from 'hook/usePagination';
-import { _axios } from 'lib/axiosInstance';
-import type { GetFreshPostReturn } from 'lib/crawl/logic/post';
-import { names } from 'lib/crawl/targetInfo';
+import type { api_log } from '@prisma/client';
 import { getRecentAccessLog } from 'lib/log';
 import { GetServerSidePropsContext } from 'next';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
-import { KeyboardEvent, useCallback, useEffect, useState } from 'react';
 import { getSelectorsByUserAgent } from 'react-device-detect';
-import useSWR from 'swr';
 
 type Props = {
 	isMobile: boolean;
 	recentAccessLog?: api_log;
 };
-const MobileContainer = dynamic(() => import('containers/page/MobileContainer'), {});
-const DesktopContainer = dynamic(() => import('containers/page/DesktopContainer'), {});
+const MobileContainer = dynamic(() => import('containers/page/landingPage/MobileContainer'), {});
+const DesktopContainer = dynamic(() => import('containers/page/landingPage/DesktopContainer'), {});
 
-const Home = ({ isMobile = true, recentAccessLog }: Props) => {
-	const {
-		pageIdx,
-		limit,
-		order,
-		actions: { pageIndexHandler },
-		search,
-	} = usePagination({});
-
-	const { data, error, isValidating } = useSWR<GetFreshPostReturn>(
-		`/api/crawl/getFreshPost/${pageIdx}/${order.orderByHit}/${search.searchText}`,
-		async () =>
-			await _axios
-				.post(`/api/crawl/getFreshPost`, {
-					orderByHit: order.orderByHit,
-					limit,
-					offset: (pageIdx - 1) * limit,
-					searchText: search.searchText,
-				})
-				.then((res) => res.data)
-	);
-
-	const [totalCount, setTotalCount] = useState(0);
-	const [freshPostList, setFreshPostList] = useState<fresh_post[]>([]);
-
-	useEffect(() => {
-		if (data?.list && data?.list?.length > 0) {
-			setFreshPostList(data.list);
-		}
-	}, [data?.list]);
-
-	useEffect(() => {
-		if (data?.totalCount && data?.totalCount > 0 && data?.totalCount !== totalCount) {
-			setTotalCount(data.totalCount);
-		}
-	}, [data?.totalCount, totalCount]);
-
-	const pageOnKeyDownHandler = useCallback(
-		(e: KeyboardEvent<HTMLElement>) => {
-			if (e.key === 'ArrowLeft' && pageIdx > 1) pageIndexHandler(pageIdx - 1);
-			if (e.key === 'ArrowRight' && pageIdx <= Math.ceil(totalCount / limit)) pageIndexHandler(pageIdx + 1);
-			return;
-		},
-		[limit, pageIdx, pageIndexHandler, totalCount]
-	);
-
+const LandingPage = ({ isMobile = true, recentAccessLog }: Props) => {
 	return (
 		<>
 			<div>
@@ -75,40 +22,16 @@ const Home = ({ isMobile = true, recentAccessLog }: Props) => {
 					<link rel='icon' href='/favicon.ico' />
 					<meta name='viewport' content='initial-scale=1, width=device-width' />
 				</Head>
-				<main tabIndex={0} onKeyDown={pageOnKeyDownHandler}>
-					{isMobile && (
-						<MobileContainer
-							{...order}
-							{...search}
-							recentAccessLog={recentAccessLog}
-							totalCount={totalCount}
-							targetSiteCount={Object.keys(names).length}
-							limit={limit}
-							pageIdx={pageIdx}
-							pageIndexHandler={pageIndexHandler}
-							freshPostList={freshPostList}
-						/>
-					)}
-					{!isMobile && (
-						<DesktopContainer
-							{...order}
-							{...search}
-							recentAccessLog={recentAccessLog}
-							totalCount={totalCount}
-							targetSiteCount={Object.keys(names).length}
-							limit={limit}
-							pageIdx={pageIdx}
-							pageIndexHandler={pageIndexHandler}
-							freshPostList={freshPostList}
-						/>
-					)}
+				<main>
+					{isMobile && <MobileContainer />}
+					{!isMobile && <DesktopContainer />}
 				</main>
 			</div>
-			{isValidating && <CustomLoading />}
+			{/* {isValidating && <CustomLoading />} */}
 		</>
 	);
 };
-export default Home;
+export default LandingPage;
 
 export async function getServerSideProps({ req, res }: GetServerSidePropsContext) {
 	const userAgent = req.headers['user-agent'] || '';
