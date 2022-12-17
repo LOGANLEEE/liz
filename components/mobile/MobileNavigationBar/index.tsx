5ce232969e037b67caca7a3ca2a172bcf9c3a0e2
LOGANLEEE <logan_lee@kakao.com>
2022-12-17 14:40:10 +0000

    fix toggle problem

--- components/mobile/MobileNavigationBar/index.tsx
+++ components/mobile/MobileNavigationBar/index.tsx
@@ -9,13 +9,29 @@ import styled from 'styled-components';
 type Props = {};
 export const MobileNavigationBar = () => {
 	const [isToggled, setIsToggled] = useState(false);
-	console.log('isToggled:', isToggled);
 
 	return (
-		<Navbar isBordered variant='sticky' maxWidth={'fluid'}>
-			<Wrapper direction='row' gap={1} justify='flex-start'>
-				<Grid>
+		<Navbar isBordered variant='sticky' maxWidth={'fluid'} height={40}>
+			<Wrapper direction='row' gap={1} justify='center'>
+				<Grid className='item' xs={1}></Grid>
+				<Grid className='item' xs={10} justify='center'>
+					<Navbar.Brand onClick={() => (window.location.href = '/')}>
+						{/* <AcmeLogo /> */}
+						<Image
+							src='/images/l.jpg'
+							// src='/images/l.svg'
+							placeholder='blur'
+							blurDataURL={rgbDataURL(237, 181, 6)}
+							alt='Picture of the symbol'
+							width={38}
+							height={38}
+						/>
+					</Navbar.Brand>
+				</Grid>
+
+				<Grid className='item' xs={1}>
 					<Navbar.Toggle
+						isSelected={isToggled}
 						className='toggle-button'
 						onChange={(isSelected) => {
 							if (typeof isSelected === 'boolean') {
@@ -24,44 +40,41 @@ export const MobileNavigationBar = () => {
 						}}
 					/>
 				</Grid>
-				<Grid>
-					<Navbar.Brand onClick={() => (window.location.href = '/')}>
-						{/* <AcmeLogo /> */}
-						<Image
-							src='/images/l.jpg'
-							placeholder='blur'
-							blurDataURL={rgbDataURL(237, 181, 6)}
-							alt='Picture of the author'
-							width={40}
-							height={40}
-						/>
-					</Navbar.Brand>
-				</Grid>
-
-				{/* <Grid>
-					<Navbar.Content>
-					</Navbar.Content>
-				</Grid> */}
 			</Wrapper>
 			{isToggled && (
-				<Navbar.Collapse>
+				<StyledCollapse>
 					{NavigationItems.map(({ href, name }, index) => (
 						<Navbar.CollapseItem key={name}>
-							<Link color='inherit' href={href} onClick={() => setIsToggled(false)}>
+							<Link className='link' color='primary' href={href} onClick={() => setIsToggled(false)}>
 								{name}
 							</Link>
 						</Navbar.CollapseItem>
 					))}
-				</Navbar.Collapse>
+				</StyledCollapse>
 			)}
 		</Navbar>
 	);
 };
+
+const StyledCollapse = styled(Navbar.Collapse)`
+	.link {
+		width: 100%;
+		text-align: end;
+	}
+`;
 const Wrapper = styled(Grid.Container)`
 	z-index: 201;
+	margin: 0;
+	padding: 0;
+
+	.item {
+		padding-top: 0;
+		padding-bottom: 0;
+	}
+
 	.toggle-button {
-		width: 30px;
-		height: 30px;
+		width: 38px;
+		height: 38px;
 	}
 `;
 
