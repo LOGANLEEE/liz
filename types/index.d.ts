261f9f6241b97df9892ff43c6fbe61313e8911d8
LOGANLEEE <logan_lee@kakao.com>
2022-12-19 23:40:06 +0000

    add word

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,27 @@
+import type { fresh_post } from '@prisma/client';
+
 export declare type VisitedList = {
 	title?: string;
 	id: number;
 	name: string;
 }[];
+
+export declare type ChartData = {
+	avg: number;
+	count: number;
+	max: number;
+	min: number;
+	sum: number;
+	name: string;
+};
+
+export declare type ChartComponentProps = {
+	introText?: string;
+	height?: number;
+	width?: number;
+	syncId?: string;
+	XdataKey?: string;
+	YdataKey?: string;
+	dataKeys?: string[];
+	chartData: ChartData[];
+};
