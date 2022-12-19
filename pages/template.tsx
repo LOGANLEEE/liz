b072df8972e53004db25f957e691a057809a6900
LOGANLEEE <logan_lee@kakao.com>
2022-12-19 13:49:11 +0000

    update template

--- pages/template.tsx
+++ pages/template.tsx
@@ -1,11 +1,22 @@
-const Template = () => {
-	<div>about</div>;
+import { GetServerSidePropsContext } from 'next';
+import styled from 'styled-components';
+
+// const MobileContainer = dynamic(() => import('containers/page/landingPage/MobileContainer'), {});
+// const DesktopContainer = dynamic(() => import('containers/page/landingPage/DesktopContainer'), {});
+
+type Props = {
+	some?: unknown;
+};
+const Template = ({ some }: Props) => {
+	return <Wrapper>about</Wrapper>;
 };
 
-export default Template;
+const Wrapper = styled.div``;
 
-export async function getStaticProps(context: any) {
+export async function getServerSideProps({ req, res }: GetServerSidePropsContext) {
 	return {
 		props: {}, // will be passed to the page component as props
 	};
 }
+
+export default Template;
