ffb2e3aff4762b3ef80416e710ba34180801c299
LOGANLEEE <logan_lee@kakao.com>
2022-12-21 23:08:41 +0000

    add comp

---
+++ components/TargetSiteSelector/index.tsx
@@ -0,0 +1,68 @@
+import { Grid, Text } from '@nextui-org/react';
+import { names } from 'lib/crawl/targetInfo';
+import { useCallback } from 'react';
+import styled from 'styled-components';
+
+type Props = {
+	targetSiteHandler: (val: string) => void;
+	selectedSites: string[];
+};
+
+const TargetSiteSelector = ({ selectedSites, targetSiteHandler }: Props) => {
+	const onClickHandler = useCallback(
+		(val: string) => {
+			targetSiteHandler(val);
+		},
+		[targetSiteHandler]
+	);
+
+	return (
+		<Wrapper justify='flex-start' direction='column' gap={0.5}>
+			<Grid>
+				<Text className='text'>보고자하는 사이트를</Text>
+				<Text className='text'>골라보세요.</Text>
+			</Grid>
+			<Grid>
+				<StyledUl>
+					{Object.values(names).map((name) => (
+						<li
+							className={`item ${selectedSites.find((site) => site === name) ? 'selected' : ''}`}
+							key={name}
+							onClick={() => onClickHandler(name)}
+						>
+							{name}
+						</li>
+					))}
+				</StyledUl>
+			</Grid>
+		</Wrapper>
+	);
+};
+
+const Wrapper = styled(Grid.Container)`
+	.text {
+		/* text-align: center; */
+	}
+`;
+
+const StyledUl = styled.ul`
+	width: 100%;
+	.selected {
+		background-color: #9b5b5b;
+	}
+	.item {
+		cursor: pointer;
+		padding: 5px 5px 5px 5px;
+		margin: 5px 5px 5px 5px;
+		display: inline-block;
+		border: 1px solid silver;
+		border-radius: 15px;
+
+		&:hover {
+			background-color: #bbbbbb;
+			color: #000000;
+		}
+	}
+`;
+
+export default TargetSiteSelector;
