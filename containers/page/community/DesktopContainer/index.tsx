8374f5b3ebd3425cf5dcc5ed3e51a16971fb1f2e
LOGANLEEE <logan_lee@kakao.com>
2022-12-22 21:31:42 +0000

    update style

--- containers/page/community/DesktopContainer/index.tsx
+++ containers/page/community/DesktopContainer/index.tsx
@@ -1,13 +1,13 @@
 import { FormElement, Grid, Input } from '@nextui-org/react';
 import type { api_log, fresh_post } from '@prisma/client';
-import TargetSiteSelector from 'components/TargetSiteSelector';
 import { OrderBy } from 'lib/crawl/logic/post';
 import dynamic from 'next/dynamic';
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 
+const TargetSiteSelector = dynamic(() => import('components/TargetSiteSelector'), {});
 const InfoText = dynamic(() => import('components/InfoText'), {});
 const PostContainer = dynamic(() => import('containers/PostContainer'), {});
-const PostOrder = dynamic(() => import('components/PostOrder'), {});
+const PostOrder = dynamic(() => import('components/PostOrderButton'), {});
 const PaginationComp = dynamic(() => import('components/PaginationComp'), {});
 
 type Props = {
@@ -21,7 +21,7 @@ type Props = {
 	toggleOrderByHit: () => void;
 	searchText: string;
 	clearSearchText: () => void;
-	searchTextHandler: (e: React.ChangeEvent<FormElement>) => void;
+	searchTextHandler: (e: ChangeEvent<FormElement>) => void;
 	pageIndexHandler: (pageNum: number) => void;
 	selectedSites: string[];
 	targetSiteHandler: (val: string) => void;
@@ -61,9 +61,8 @@ const DesktopContainer = ({
 							initialValue={searchText}
 							underlined
 							placeholder='Search...'
-							width='90%'
+							width='100%'
 						/>
-						<PostOrder orderByHit={orderByHit} toggleOrderByHit={toggleOrderByHit} />
 					</Grid>
 					<Grid xs={12} sm={12} md={12} lg={12} xl={12} justify='center'>
 						<PaginationComp limit={limit} totalCount={totalCount} page={pageIdx} onChangeHandler={pageIndexHandler} />
@@ -77,8 +76,9 @@ const DesktopContainer = ({
 					</Grid>
 				</Grid.Container>
 			</Grid>
-			<Grid xs={1} sm={2} md={1} lg={1} xl={1}>
-				<TargetSiteSelector selectedSites={selectedSites} targetSiteHandler={targetSiteHandler} />
+			<Grid xs={1} sm={2} md={1} lg={1} xl={1} direction='column'>
+				<PostOrder orderByHit={orderByHit} toggleOrderByHit={toggleOrderByHit} />
+				<TargetSiteSelector useTitle selectedSites={selectedSites} targetSiteHandler={targetSiteHandler} />
 			</Grid>
 		</Grid.Container>
 	);
