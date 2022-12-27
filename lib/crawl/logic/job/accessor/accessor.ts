8ecc4eb8bb93635481b82abeb7991c3337627da6
LOGANLEEE <logan_lee@kakao.com>
2022-12-27 22:57:20 +0000

    add file and update

---
+++ lib/crawl/logic/job/accessor/accessor.ts
@@ -0,0 +1,127 @@
+import { Prisma } from '@prisma/client';
+import { load } from 'cheerio';
+import { format } from 'date-fns';
+import { _axiosCrawler } from 'lib/axiosInstance';
+import { TargetJobSiteInfo } from 'lib/crawl/targetJobSiteInfo';
+import { communityNames } from 'lib/crawl/targetSiteInfo';
+import { humorUniCookie } from 'lib/util';
+
+type UniversalAccessorArgs = {
+	// page: Page;
+	targetInfo: TargetJobSiteInfo;
+	pageCount: number;
+};
+
+type UniversalAccessorReturn = {
+	list: Prisma.fresh_[];
+	isError: boolean;
+	message: string;
+	name: string;
+};
+
+export const accessor = async ({ targetInfo, pageCount }: UniversalAccessorArgs): Promise<UniversalAccessorReturn> => {
+	let isError = false;
+
+	const tempHolder: Prisma.fresh_postCreateInput[] = [];
+	const result = (message: string) => ({ list: tempHolder, isError, message, name: targetInfo.name });
+
+	const headers: any = {};
+
+	if (targetInfo.name === communityNames.ud) {
+		headers.Cookie = humorUniCookie;
+	}
+
+	const { data, err } = await _axiosCrawler
+		.get(targetInfo.targetUrl(pageCount), {
+			headers,
+		})
+		.then((res) => ({ ...res, err: false }))
+		.catch((err) => ({ err, data: undefined }));
+
+	if (!data && err) {
+		isError = true;
+		return result(`axios error: ${JSON.stringify(err)}`);
+	}
+
+	// await writeFile('./dummy.html', data);
+
+	try {
+		const $ = load(data);
+
+		for (let postCount = targetInfo.postRange[0]; postCount <= targetInfo.postRange[1]; postCount += targetInfo.postRange[2]) {
+			if (targetInfo?.targetIndex) {
+				if (targetInfo?.targetIndex(postCount) !== '') {
+					const postIndex = $(targetInfo?.targetIndex(postCount))?.text()?.trim();
+					if (isNaN(parseInt(postIndex))) continue;
+				}
+			}
+
+			// remove garbage tag
+			if (targetInfo?.garbage) {
+				targetInfo.garbage(postCount)?.forEach((path) => {
+					if (path) {
+						$(path)?.remove();
+					}
+				});
+			}
+			let title = $(targetInfo.title ? targetInfo.title(postCount) : targetInfo.link(postCount))
+				.text()
+				?.trim();
+
+			if (targetInfo.titleHandler && title) {
+				title = targetInfo.titleHandler(title);
+			}
+
+			let link = $(targetInfo.link(postCount))?.attr('href');
+
+			if (targetInfo.linkHandler && link) {
+				link = targetInfo.linkHandler(link);
+			}
+
+			const author = $(targetInfo.author(postCount))?.text()?.trim();
+
+			const hit = parseInt(
+				$(targetInfo.hit(postCount))
+					?.text()
+					?.trim()
+					?.replaceAll(' ', '')
+					.replaceAll(',', '')
+					.replaceAll('.', '')
+					.replaceAll('k', '00')
+					.replaceAll('조회', ''),
+				10
+			);
+
+			// console.log('title:', title);
+			// console.log('link:', `${targetInfo.targetBaseName}${link}`);
+			// console.log('hit:', isNaN(hit) ? -1 : hit);
+			// console.log('author:', author);
+
+			if (!title && !link) continue;
+
+			tempHolder.push({
+				title,
+				link: `${targetInfo.targetBaseName}${link}`,
+				hit: isNaN(hit) ? -1 : hit,
+				name: targetInfo.name,
+				author,
+				mark: false,
+				content: null,
+			});
+		}
+		if (tempHolder.length < 1) {
+			isError = true;
+			throw 'holder length 0';
+		}
+		return result('good');
+	} catch (error) {
+		isError = true;
+		// console.log('parsing error: ', error);
+
+		return result(`selector error ${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}`);
+	}
+};
+
+// await writeFile('./dummy.html', await newPage.content()).then(() => {
+// 	console.log('file write.');
+// });
--- lib/crawl/logic/job/runner/parallelRunner.ts
+++ lib/crawl/logic/job/runner/parallelRunner.ts
@@ -1,19 +1,16 @@
 import { format } from 'date-fns';
-import { pptrAccessor } from 'lib/crawl/logic/accessor/pptrAccessor';
+import { accessor } from 'lib/crawl/logic/community/accessor/axios/accessor';
 import { afterStageCleanUp } from 'lib/crawl/logic/cleaner';
 import { targetSiteList } from 'lib/crawl/targetSiteInfo';
 import { writeLog } from 'lib/log';
-import { getBrowser } from 'lib/pptrInstace';
 import { serverState } from 'lib/state';
-import { measure } from 'lib/util';
+import { delay, measure } from 'lib/util';
 import { _prisma } from 'prisma/prismaInstance';
 
-export const pptrParallelRunner = async () => {
+export const parallelRunner = async () => {
 	serverState.isCrawling = true;
 
 	serverState.listStatus = serverState.listStatus.map((e) => ({ ...e, on: false }));
-
-	const { browser } = await getBrowser();
 
 	const stage1startTime = performance.now();
 
@@ -27,8 +24,22 @@ export const pptrParallelRunner = async () => {
 
 			const st = performance.now();
 			const pageHolder = await Promise.all(
-				pageRange.map(async (pageCount) => await pptrAccessor({ targetInfo, pageCount, browser }))
+				pageRange.map(async (pageCount, idx) => {
+					await delay(idx * 800);
+					const firstResult = await accessor({ targetInfo, pageCount });
+					// if (firstResult.isError) {
+					// 	return await pptrAccessor({ pageCount, targetInfo, browser: (await getBrowser()).browser });
+					// }
+					return firstResult;
+				})
 			);
+
+			// const pageHolder = [];
+			// for (let pageCount = targetInfo.pageRange[0]; pageCount <= targetInfo.pageRange[1]; pageCount += targetInfo.pageRange[2]) {
+			// 	await delay(300 * pageCount);
+			// 	const result = await axiosUniversalAccessor({ targetInfo, pageCount });
+			// 	pageHolder.push(result);
+			// }
 
 			// writeFile('./parallel.jsonc', JSON.stringify(pageHolder?.map(({ list }) => list).flat()));
 
@@ -42,10 +53,15 @@ export const pptrParallelRunner = async () => {
 				if (e.name === targetInfo.name) e.on = true;
 				return e;
 			});
+			const isError = pageHolder.some((e) => e.isError === true);
 
-			console.log(`${targetInfo.name}: ${measure(st, performance.now(), 1000)} sec ${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}`);
+			console.log(
+				`${targetInfo.name}(${targetInfo.pageRange[1]}): (${count} /${
+					((targetInfo.postRange[1] - targetInfo.postRange[0] + 1) / targetInfo.postRange[2]) * targetInfo.pageRange[1]
+				})${isError ? 'error' : 'pass'} ${measure(st, performance.now(), 1000)} sec ${format(new Date(), 'yyyy-MM-dd HH:mm:ss')}`
+			);
 
-			return { count, isError: pageHolder.some((e) => e.isError), name: targetInfo.name };
+			return { count, isError, name: targetInfo.name, message: JSON.stringify(pageHolder.map((e) => e.message)) };
 		})
 	);
 
@@ -66,8 +82,6 @@ export const pptrParallelRunner = async () => {
 			serverState.isCrawling = false;
 			serverState.listStatus = serverState.listStatus.map((e) => ({ ...e, on: false }));
 		});
-
-	await browser.close();
 
 	// tempHolder?.map((e: any) => console.log(e?.info));
 	// writeFile('dummy1.jsonc', JSON.stringify(tempHolder));
